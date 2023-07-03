05b62e0c37d26742d847e7ab32cf344d0471da25
Samuel <smltrs0@gmail.com>
2023-07-03 10:28:31 -0500

    feat: Fix unit test for error handling

--- frontend/src/components/Register/RegisterForm.test.js
+++ frontend/src/components/Register/RegisterForm.test.js
@@ -1,76 +1,78 @@
+import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import RegisterForm from "./RegisterForm";
 
 describe("RegisterForm", () => {
-  it("renders without error", () => {
-    render(<RegisterForm />);
-  });
-
-  it("updates name value correctly", () => {
-    const { getByLabelText } = render(<RegisterForm />);
-    const nameInput = getByLabelText("Usuario");
-
-    fireEvent.change(nameInput, { target: { value: "John Doe" } });
-
-    expect(nameInput.value).toBe("John Doe");
-  });
-
-  it("updates email value correctly", () => {
-    const { getByLabelText } = render(<RegisterForm />);
-    const emailInput = getByLabelText("Correo Electrónico");
-
-    fireEvent.change(emailInput, { target: { value: "test@example.com" } });
-
-    expect(emailInput.value).toBe("test@example.com");
-  });
-
-  it("updates password value correctly", () => {
-    const { getByLabelText } = render(<RegisterForm />);
-    const passwordInput = getByLabelText("Contraseña");
-
-    fireEvent.change(passwordInput, { target: { value: "password123" } });
-
-    expect(passwordInput.value).toBe("password123");
-  });
-
-  it("updates confirm password value correctly", () => {
-    const { getByLabelText } = render(<RegisterForm />);
-    const confirmPasswordInput = getByLabelText("Confirmar Contraseña");
-
-    fireEvent.change(confirmPasswordInput, { target: { value: "password123" } });
-
-    expect(confirmPasswordInput.value).toBe("password123");
-  });
-
-  it("calls onSubmit with correct userdata on form submission", () => {
-    const mockOnSubmit = jest.fn();
-    const { getByLabelText, getByText } = render(
-      <RegisterForm onSubmit={mockOnSubmit} />
+  it("should update name field value", () => {
+    const dataForm = { name: "", email: "", password: "", password_confirmation: "" };
+    const handlerDataFor = jest.fn();
+    const { getByLabelText } = render(
+      <RegisterForm handlerDataFor={handlerDataFor} dataForm={dataForm} />
     );
 
-    fireEvent.change(getByLabelText("Usuario"), { target: { value: "John Doe" } });
-    fireEvent.change(getByLabelText("Correo Electrónico"), {
-      target: { value: "test@example.com" },
-    });
-    fireEvent.change(getByLabelText("Contraseña"), { target: { value: "password123" } });
-    fireEvent.change(getByLabelText("Confirmar Contraseña"), {
-      target: { value: "password123" },
-    });
+    const nameInput = getByLabelText("Usuario");
+    fireEvent.change(nameInput, { target: { value: "test test" } });
 
-    fireEvent.click(getByText("Registrarse"));
-
-    expect(mockOnSubmit).toHaveBeenCalledWith({
-      name: "John Doe",
-      email: "test@example.com",
-      password: "password123",
-      password_confirmation: "password123",
-    });
+    expect(handlerDataFor).toHaveBeenCalledWith({ name: "test test", email: "", password: "", password_confirmation: "" });
   });
 
-  it("displays error message when error prop is provided", () => {
-    const { getByText } = render(<RegisterForm error="Error message" />);
-    const errorMessage = getByText("Error message");
+  it("should update email field value", () => {
+    const dataForm = { name: "", email: "", password: "", password_confirmation: "" };
+    const handlerDataFor = jest.fn();
+    const { getByLabelText } = render(
+      <RegisterForm handlerDataFor={handlerDataFor} dataForm={dataForm} />
+    );
 
-    expect(errorMessage).toBeInTheDocument();
+    const emailInput = getByLabelText("Correo Electrónico");
+    fireEvent.change(emailInput, { target: { value: "john@example.com" } });
+
+    expect(handlerDataFor).toHaveBeenCalledWith({ name: "", email: "john@example.com", password: "", password_confirmation: "" });
   });
+
+  it("should update password field value", () => {
+    const dataForm = { name: "", email: "", password: "", password_confirmation: "" };
+    const handlerDataFor = jest.fn();
+    const { getByLabelText } = render(
+      <RegisterForm handlerDataFor={handlerDataFor} dataForm={dataForm} />
+    );
+
+    const passwordInput = getByLabelText("Contraseña");
+    fireEvent.change(passwordInput, { target: { value: "password123" } });
+
+    expect(handlerDataFor).toHaveBeenCalledWith({ name: "", email: "", password: "password123", password_confirmation: "" });
+  });
+
+  it("should update confirmPassword field value", () => {
+    const dataForm = { name: "", email: "", password: "", password_confirmation: "" };
+    const handlerDataFor = jest.fn();
+    const { getByLabelText } = render(
+      <RegisterForm handlerDataFor={handlerDataFor} dataForm={dataForm} />
+    );
+
+    const confirmPasswordInput = getByLabelText("Confirmar Contraseña");
+    fireEvent.change(confirmPasswordInput, { target: { value: "password123" } });
+
+    expect(handlerDataFor).toHaveBeenCalledWith({ name: "", email: "", password: "", password_confirmation: "password123" });
+  });
+
+  it("should call onSubmit when the form is submitted", () => {
+    const dataForm = { name: "", email: "", password: "", password_confirmation: "" };
+    const handlerDataFor = jest.fn();
+    const onSubmit = jest.fn();
+    const { getByText } = render(
+      <RegisterForm
+        onSubmit={onSubmit}
+        handlerDataFor={handlerDataFor}
+        dataForm={dataForm}
+      />
+    );
+
+    const submitButton = getByText("Registrarse");
+    fireEvent.click(submitButton);
+
+    expect(onSubmit).toHaveBeenCalledWith(dataForm);
+  });
+  
+  // Aquí puedes agregar más pruebas unitarias según tus necesidades
+  
 });
